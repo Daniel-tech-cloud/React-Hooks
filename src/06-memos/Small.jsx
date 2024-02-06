235ce8b9066a45803998ab5152a75940a6e24259
Daniel <guardians01@outlook.com>
2024-02-06 16:01:27 -0600

    Implementación de useCallback

--- src/06-memos/Small.jsx
+++ src/06-memos/Small.jsx
@@ -1,19 +1,19 @@
-// import { memo } from "react";
+import { memo } from "react";
 
-// export const Small = memo (({value}) => {
-//     console.log('Me volví a redibijar :(');
-//   return (
-//     <small> {value} </small>
-//   )
-// })
-
-import React from "react";
-
-export const Small = React.memo (({value}) => {
+export const Small = memo (({value}) => {
     console.log('Me volví a redibijar :(');
   return (
     <small> {value} </small>
   )
 })
+
+// import React from "react";
+
+// export const Small = React.memo (({value}) => {
+//     console.log('Me volví a redibijar :(');
+//   return (
+//     <small> {value} </small>
+//   )
+// })
 
 
