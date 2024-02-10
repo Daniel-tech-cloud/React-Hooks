e6aeb08cb519f9ff69ce6e7e013899612ab24239
Daniel Barrera <guardians01@outlook.com>
2024-02-10 15:59:56 -0600

    Implementación de router

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 // import { HooksApp } from './HooksApp';
 // import { CounterApp } from './01-useSate/CounterApp';
 // import { CounterWithCustomHook } from './01-useSate/CounterWithCustomHook';
@@ -15,10 +17,16 @@ import ReactDOM from 'react-dom/client';
 // import './08-useReducer/intro-reducer';
 import './index.css';
 import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <TodoApp />
-  /* </React.StrictMode> */
+  <BrowserRouter>
+  
+  {/* <React.StrictMode> */}
+      <MainApp />
+  {/* </React.StrictMode> */}
+
+  </BrowserRouter>
+
 )
